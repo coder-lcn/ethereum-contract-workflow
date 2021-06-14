4d89d29e55c8852aa75f689de6c78a36981d045b
LiChangNan-Programmer <1690997438@qq.com>
2021-06-15 03:25:58 +0800

    【进阶篇 2：编写智能合约编译脚本：compile】改造完成

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -1,11 +1,17 @@
-import fs from "fs";
+import fs from "fs-extra";
 import path from "path";
 import solc from "solc";
 
+// cleanup
+const compiledDir = path.resolve(__dirname, "../compiled");
+fs.removeSync(compiledDir);
+fs.ensureDirSync(compiledDir);
+
+// compile
 const contractPath = path.resolve(__dirname, "../contracts", "Car.sol");
 const contractSource = fs.readFileSync(contractPath, "utf8");
 
-var input = {
+const input = {
   language: "Solidity",
   sources: {
     "Car.sol": {
@@ -21,6 +27,17 @@ var input = {
   },
 };
 
-var output = JSON.parse(solc.compile(JSON.stringify(input)));
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-console.log(output.contracts["Car.sol"].Car.abi);
+// check errors
+if (Array.isArray(output.errors) && output.errors.length) {
+  throw new Error(output.errors[0].formattedMessage);
+}
+
+Object.keys(output.contracts).forEach((name) => {
+  const contractName = name.split(".")[0];
+  const filePath = path.resolve(compiledDir, `${contractName}.json`);
+
+  fs.outputJsonSync(filePath, output.contracts[name][contractName]);
+  console.log(`save compiled contract ${contractName} to ${filePath}`);
+});
